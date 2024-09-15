5b94a799a2a1a69fec9b82c5224e1a6291b6f061
lx-jdar <jdar.soft@gmail.com>
2024-09-15 03:33:30 -0300

    adding first logic for state machine

--- Arduino/SmartSaw.ino
+++ Arduino/SmartSaw.ino
@@ -4,7 +4,7 @@
 #define PULSADOR_IZQ 5
 
 // Puente H
-#define PM_SPEED 6			// Modulacion del PWM M1 
+#define PM_SPEED 6    // Modulacion del PWM M1 
 #define M_T1 7				// Control Motor P2 L293D, Terminal 1
 #define M_T2 8				// Control Motor P2 L293D, Terminal 2
 
@@ -21,67 +21,211 @@
 #define TRIGGER_V 12
 #define ECHO_V 13
 
-// Estados del embebido
-#define IDLE 14
-#define EN_MOVIMIENTO 15
-#define SIERRA_ACTIVA 16
+// Estados del Embebido
+#define ESTADO_IDLE 14
+#define ESTADO_EN_MOVIMIENTO 15
+#define ESTADO_SIERRA_ACTIVA 16
 
-// Tolerancia de longitud para apagar el motor (REVISAR)
+// Tolerancia de longitud para apagar el motor
 #define DISTANCIA_MINIMA_V 150
 
-int MOTOR_SPEED = 128;
+// --------------------------------------------------------------------------
+// REVISAR LOGICA (CUANDO SE VA DESPLAZANDO SUMA EL TOPE Y NO DESPLAZA DEL TODO)
+// VER FUNCION checkearPosicionMotor
+#define DISTANCIA_MAXIMA_H 200
+// --------------------------------------------------------------------------
 
-int estadoEmbebido = IDLE;
-long MARGEN_DE_ERROR = 1;
+#define MARGEN_DE_ERROR 1L
 
-bool imprimirMsg;
+#define ULTRASONIDO_EN_MOVIMIENTO true
+#define ULTRASONIDO_SIN_MOVIMIENTO false
 
-// Variable para almacenar el valor de desplazamiento del motor en cm
-int valorDesplazamiento = 0;
+#define BAUD_RATE 9600
 
-// Estado del motor (encendido/apagado)
-bool sierraEncendida = false;
+//INTERRUPCIONES
 
-// Para detectar si el botón fue presionado
-bool botonSierraPresionado = false;
+#define INTERRUPCION_SIERRA digitalPinToInterrupt(PULSADOR_SIERRA)
 
-// Para calcular la posicion actual del ultrasonido horizontal
-long posicionActual = 0;
+//MOTOR
+#define MOTOR_SPEED 128
 
-int posicionDePartida;
+//ULTRASONIDO
 
-int pulsadorPresionado;
+#define CONST_VELOCIDAD_SONIDO 57
 
-//----------------------------------------------------
-// Arduino Setup
-//----------------------------------------------------
-void setup()
+//MODO INTERRUPCION
+
+#define MODO_PIN_SIERRA RISING
+
+bool interrupcionDetectada = false;
+
+int estadoEmbebido = ESTADO_IDLE;
+
+struct structEvento
 {
-  inicializarHardware();
-  imprimirMsg = true;
-}
+  int tipo;
+  int valor;
+};
 
+
+bool imprimirMsg = true;
+int pulsadorPresionado;
+int valorDesplazamiento = 0;    // Variable para almacenar el valor de desplazamiento del motor en cm
+int posicionActual = 0;
+int posicionDePartida = 0;
+
+//Maquina de estado
+int current_state=0;
+structEvento evento;
 //----------------------------------------------------
 // Arduino Loop
 //----------------------------------------------------
-void loop()
+
+
+void getEvento()
 {
-  msf();
+   if(esPulsadorPresionado(PULSADOR_IZQ))
+   {
+		evento.tipo=PULSADOR_IZQ;
+   }
+   if(esPulsadorPresionado(PULSADOR_DER))
+   {
+		evento.tipo=PULSADOR_DER;
+   }
+   if(esPulsadorPresionado(PULSADOR_SIERRA))
+   {
+		evento.tipo=PULSADOR_SIERRA;
+   }
+   
+}
+void maquinaEstado()
+{
+   switch (estadoEmbebido)
+    {
+      case ESTADO_IDLE:
+      {
+          leerTiempoDesdeSerie();
+          getEvento();
+          switch (evento.tipo)
+          {
+              case PULSADOR_IZQ:
+              {
+					estadoEmbebido = ESTADO_EN_MOVIMIENTO;
+                    Serial.println("Pulsador izquierdo presionado. Desplazando motor a izquierda.");
+                    posicionDePartida = medirDistanciaUltrasonido(TRIGGER_H, ECHO_H, ULTRASONIDO_SIN_MOVIMIENTO, PULSADOR_IZQ);
+                    encenderMotorDesplazamiento(PULSADOR_IZQ);
+                    imprimirMsg = true;
+                    pulsadorPresionado = PULSADOR_IZQ;
+              }
+              break;
+            
+              case PULSADOR_DER:
+              {
+                    estadoEmbebido = ESTADO_EN_MOVIMIENTO;
+                    Serial.println("Pulsador derecho presionado. Desplazando motor a derecha.");
+                    posicionDePartida = medirDistanciaUltrasonido(TRIGGER_H, ECHO_H, ULTRASONIDO_SIN_MOVIMIENTO, PULSADOR_DER);
+                    encenderMotorDesplazamiento(PULSADOR_DER);
+                    imprimirMsg = true;
+                    pulsadorPresionado = PULSADOR_DER;
+              }
+              break;
+            
+                 case PULSADOR_SIERRA:
+              {
+                    estadoEmbebido = ESTADO_SIERRA_ACTIVA;
+                    Serial.println("Pulsador sierra presionado. Cortando ...");
+                    posicionDePartida = medirDistanciaUltrasonido(TRIGGER_V, ECHO_V, ULTRASONIDO_SIN_MOVIMIENTO, PULSADOR_SIERRA);
+                    encenderMotorYLedSierra();
+              }
+              break;
+          }
+          break;
+      }
+      break;
+     
+      case ESTADO_EN_MOVIMIENTO:
+      {
+          switch (evento.tipo)
+          {
+            case PULSADOR_IZQ:
+            {
+                checkearPosicionMotor(PULSADOR_IZQ);
+            }
+            break;
+
+            case PULSADOR_DER:
+            {
+                checkearPosicionMotor(PULSADOR_DER);
+            }
+            break;
+          }
+          break;
+      }
+      break;
+     
+     
+      case ESTADO_SIERRA_ACTIVA:
+      {
+          switch (evento.tipo)
+          {
+            case PULSADOR_SIERRA:
+            {
+                checkearLongitudDeCorte();
+            }
+            break;
+          }
+          break;
+      }
+      break;
+
+
+   }
+ 
+
+  
 }
 
-//ISF() {
-  //estadoEmbebido = IDLE;
-//}
+void ISF()
+{
+  Serial.println("Interrupcion detectada, sierra apagada.");
+ 
+  if(estadoEmbebido==ESTADO_SIERRA_ACTIVA)
+  {
+      Serial.println("TEST.");
+      detenerMotorYLedSierra();
+
+  	  estadoEmbebido = ESTADO_IDLE;
+      evento.tipo=ESTADO_IDLE;
+  //detachInterrupt(digitalPinToInterrupt(PULSADOR_SIERRA));
+    
+  }
+ 
+ 
+}
+void setup()
+{
+  inicializarHardware();
+  attachInterrupt(digitalPinToInterrupt(PULSADOR_SIERRA), ISF, MODO_PIN_SIERRA);
+  
+  
+}
+
+void loop()
+{
+ // msf();
+  maquinaEstado();
+}
 
 //----------------------------------------------------
 // Inicialización
 //----------------------------------------------------
+
 void inicializarHardware() 
 {
   // Monitor Serial
-  Serial.begin(9600);
+  Serial.begin(BAUD_RATE);
   
-  // Rele
+  // Relé
   pinMode(RELE, OUTPUT);
   
   // Puente H
@@ -102,14 +246,75 @@ void inicializarHardware()
   pinMode(TRIGGER_H, OUTPUT);
   pinMode(ECHO_H, INPUT);
   
-  // Sensor Ultrasonido Horizontal
+  // Sensor Ultrasonido Vertical
   pinMode(TRIGGER_V, OUTPUT);
   pinMode(ECHO_V, INPUT);
   
-  // Inicializar relé y LED apagados
-  apagarMotorYLed(RELE, LED_DESPLAZAMIENTO);
-  estadoEmbebido = IDLE;
+  // Inicializar motores
+  detenerMotorDesplazamiento();
+  detenerMotorYLedSierra();
+
+  // Inicalizar estado embebido
+  estadoEmbebido = ESTADO_IDLE;
 }
+
+//----------------------------------------------------
+// Interrupciones
+//----------------------------------------------------
+
+//----------------------------------------------------
+// Máquina de Estados
+//----------------------------------------------------
+
+void msf()
+{
+  if (estadoEmbebido == ESTADO_IDLE)
+  {
+    leerTiempoDesdeSerie();
+    if (esPulsadorPresionado(PULSADOR_IZQ))
+    {
+      estadoEmbebido = ESTADO_EN_MOVIMIENTO;
+      Serial.println("Pulsador izquierdo presionado. Desplazando motor a izquierda.");
+      posicionDePartida = medirDistanciaUltrasonido(TRIGGER_H, ECHO_H, ULTRASONIDO_SIN_MOVIMIENTO, PULSADOR_IZQ);
+      encenderMotorDesplazamiento(PULSADOR_IZQ);
+      imprimirMsg = true;
+      pulsadorPresionado = PULSADOR_IZQ;
+      evento.tipo=PULSADOR_IZQ;
+    }
+    else if (esPulsadorPresionado(PULSADOR_DER))
+    {
+      estadoEmbebido = ESTADO_EN_MOVIMIENTO;
+      Serial.println("Pulsador derecho presionado. Desplazando motor a derecha.");
+      posicionDePartida = medirDistanciaUltrasonido(TRIGGER_H, ECHO_H, ULTRASONIDO_SIN_MOVIMIENTO, PULSADOR_DER);
+      encenderMotorDesplazamiento(PULSADOR_DER);
+      imprimirMsg = true;
+      pulsadorPresionado = PULSADOR_DER;
+      evento.tipo=PULSADOR_DER;
+    } 
+    else if (esPulsadorPresionado(PULSADOR_SIERRA))
+    {
+      estadoEmbebido = ESTADO_SIERRA_ACTIVA;
+      evento.tipo=ESTADO_SIERRA_ACTIVA;
+      Serial.println("Pulsador sierra presionado. Cortando ...");
+      posicionDePartida = medirDistanciaUltrasonido(TRIGGER_V, ECHO_V, ULTRASONIDO_SIN_MOVIMIENTO, PULSADOR_SIERRA);
+      encenderMotorYLedSierra();
+    //  attachInterrupt(INTERRUPCION_SIERRA, ISF, RISING);  // Interrupción al cambio en PULSADOR_SIERRA
+	
+    } 
+  }
+  else if (estadoEmbebido == ESTADO_EN_MOVIMIENTO)
+  {
+  	checkearPosicionMotor(pulsadorPresionado);
+  }
+  else if (estadoEmbebido == ESTADO_SIERRA_ACTIVA)
+  {
+  	checkearLongitudDeCorte();
+  }
+}
+
+//----------------------------------------------------
+// Funciones Motor Desplazamiento
+//----------------------------------------------------
 
 void leerTiempoDesdeSerie()
 {
@@ -121,7 +326,6 @@ void leerTiempoDesdeSerie()
   if (Serial.available() > 0)
   {
     String input = Serial.readStringUntil('\n'); 
-    // Esto es solo de prueba, se debe modificar con el desplazamiento del motor
     valorDesplazamiento = input.toInt(); 
     if (valorDesplazamiento > 0)
     {
@@ -135,112 +339,50 @@ void leerTiempoDesdeSerie()
   }
 }
 
-void msf()
+void checkearPosicionMotor(unsigned int pinPulsador)
 {
-  
-  if (estadoEmbebido == IDLE) {
-    leerTiempoDesdeSerie();
-    if (esPulsadorPresionado(PULSADOR_IZQ))
-    {
-      estadoEmbebido=EN_MOVIMIENTO;
-      Serial.println("Pulsador izquierdo presionado. Desplazando motor a izquierda.");
-      posicionDePartida = medirDistanciaUltrasonido(TRIGGER_H, ECHO_H, false, MARGEN_DE_ERROR, 1);
-      encenderMotorDesplazamiento(PULSADOR_IZQ);
-      Serial.println("Posicionamiento finalizado.");
-      imprimirMsg = true;
-      pulsadorPresionado=PULSADOR_IZQ;
-    }
-    else if (esPulsadorPresionado(PULSADOR_DER))
-    {
-      estadoEmbebido=EN_MOVIMIENTO;
-      Serial.println("Pulsador derecho presionado. Desplazando motor a derecha.");
-      posicionDePartida = medirDistanciaUltrasonido(TRIGGER_H, ECHO_H, false, MARGEN_DE_ERROR, 1);
-      encenderMotorDesplazamiento(PULSADOR_DER);
-      Serial.println("Posicionamiento finalizado.");
-      imprimirMsg = true;
-      pulsadorPresionado=PULSADOR_DER;
-    } 
-    else if (esPulsadorPresionado(PULSADOR_SIERRA))
-    {
-      estadoEmbebido=SIERRA_ACTIVA;
-      Serial.println("Pulsador sierra presionado. Cortando.");
-      posicionDePartida = medirDistanciaUltrasonido(TRIGGER_V, ECHO_V, false, MARGEN_DE_ERROR, 1);
-      encenderMotorYLed(RELE,LED_SIERRA); // encendemos sierra
-      
-    } 
-    
-  } else if (estadoEmbebido == EN_MOVIMIENTO) {
-  	checkearPosicionMotor(pulsadorPresionado);
-  } else if (estadoEmbebido == SIERRA_ACTIVA) {
-    //actualizarEstadoMotor();
-  	checkearLongitudDeCorte();
+  posicionActual = medirDistanciaUltrasonido(TRIGGER_H, ECHO_H, ULTRASONIDO_EN_MOVIMIENTO, pinPulsador);
+  Serial.println("Posicion actual ULT H: " + String(posicionActual) + " CM.");
+  int posibleDesplazamiento = posicionActual + (pinPulsador == PULSADOR_IZQ ? -valorDesplazamiento : valorDesplazamiento);
+  if(posibleDesplazamiento >= DISTANCIA_MAXIMA_H)
+  {
+    detenerMotorDesplazamiento();
+    Serial.println("Se quiere desplazar mas que el umbral maximo. Motor de desplazamiento apagandose ...");
+    estadoEmbebido = ESTADO_IDLE;
+    evento.tipo=ESTADO_IDLE;
   }
-  
+  else
+  {
+    int delta = abs(posicionActual - posicionDePartida);
+    if (delta >= valorDesplazamiento)
+    {
+      detenerMotorDesplazamiento();
+      Serial.println("Posicionamiento finalizado.");
+      estadoEmbebido = ESTADO_IDLE;
+      evento.tipo=ESTADO_IDLE;
+    }
+    Serial.println("El motor se desplazo horizontalmente hacia la " + 
+      String(pinPulsador == PULSADOR_IZQ ? "izquierda" : "derecha") + " unos " + 
+      String(delta) + " CM de " + String(valorDesplazamiento) + " CM.");
+  }
 }
 
-bool esPulsadorPresionado(int pinPulsador)
-{
-  return digitalRead(pinPulsador) == HIGH;
-}
-
-void encenderMotorDesplazamiento(int pinPulsador)
+void encenderMotorDesplazamiento(unsigned int pinPulsador)
 {
   if (pinPulsador == PULSADOR_IZQ) 
   {
-    // Set the speed
     analogWrite(PM_SPEED, MOTOR_SPEED); 
-    // Set the rotation direction
     digitalWrite(M_T1, HIGH);
     digitalWrite(M_T2, LOW);
     digitalWrite(LED_DESPLAZAMIENTO, HIGH);
-    //desplazarMotor(pinPulsador);
-    // ELIMINAR DELAYS
-    //delay(valorDesplazamiento * 1000);
   } 
   else if (pinPulsador == PULSADOR_DER)
   {
-    // We change the rotation direction
     analogWrite(PM_SPEED, MOTOR_SPEED); 
     digitalWrite(M_T1, LOW);
     digitalWrite(M_T2, HIGH);
     digitalWrite(LED_DESPLAZAMIENTO, HIGH);
-    //desplazarMotor(pinPulsador);
-    // ELIMINAR DELAYS
-    //delay(valorDesplazamiento * 1000);
   }
-
-}
-
-void checkearLongitudDeCorte() 
-{
-  posicionActual = medirDistanciaUltrasonido(TRIGGER_V, ECHO_V, false, MARGEN_DE_ERROR, 1);
-  Serial.println("Posicion actual ULT V: " + String(posicionActual) + " CM.");
-  if (esPulsadorPresionado(PULSADOR_SIERRA) || posicionActual <= DISTANCIA_MINIMA_V)
-  {
-    Serial.println("Pase el humbral min o presione boton");
-    apagarMotorYLed(RELE, LED_SIERRA);
-    estadoEmbebido = IDLE;
-  }
-}
-
-void checkearPosicionMotor(int pinPulsador)
-{
-  posicionActual = medirDistanciaUltrasonido(TRIGGER_H, ECHO_H, EN_MOVIMIENTO, MARGEN_DE_ERROR, pinPulsador);
-
-  Serial.println("Posicion actual ULT H: " + String(posicionActual) + " CM.");
-
-  //posicionActual =  valorDesplazamiento + distancia;
-  //posicionActual += distancia;
-  //Serial.println("Posicion actual luego del desplazamiento: " + String(posicionActual) + " CM.");
-
-  
-  int delta= abs(posicionActual-posicionDePartida);
-  if (delta>=valorDesplazamiento) {
-    detenerMotorDesplazamiento();
-    estadoEmbebido = IDLE;
-  }
-  Serial.println("El motor se desplazo en horizontal unos " + String(delta) + " CM de " + String(valorDesplazamiento));
-  
 }
 
 void detenerMotorDesplazamiento()
@@ -249,23 +391,45 @@ void detenerMotorDesplazamiento()
   digitalWrite(M_T1, LOW);
   digitalWrite(M_T2, LOW);
   digitalWrite(LED_DESPLAZAMIENTO, LOW);
-  // Breve pausa para asegurarse de que el motor se detenga completamente - ELIMINAR DELAYS
-  //delay(200);
+  valorDesplazamiento = 0;  // Limpiar la variable desplazamiento
+   Serial.println("Deteniendo el motor desplazamiento");
 }
 
-void encenderMotorYLed(int pinMotor, int pinLed)
+//----------------------------------------------------
+// Funciones Motor Sierra
+//----------------------------------------------------
+
+void checkearLongitudDeCorte() 
 {
-  digitalWrite(pinMotor, HIGH);
-  digitalWrite(pinLed, HIGH);
+  posicionActual = medirDistanciaUltrasonido(TRIGGER_V, ECHO_V, ULTRASONIDO_SIN_MOVIMIENTO, PULSADOR_SIERRA);
+  Serial.println("Posicion actual ULT V: " + String(posicionActual) + " CM.");
+  if ( posicionActual <= DISTANCIA_MINIMA_V) //esPulsadorPresionado(PULSADOR_SIERRA) ||
+  {
+    Serial.println("Pase el umbral minimo o presione el boton. Motor de sierra apagandose ...");
+    detenerMotorYLedSierra();
+    estadoEmbebido = ESTADO_IDLE;
+    evento.tipo=ESTADO_IDLE;
+  }
 }
 
-void apagarMotorYLed(int pinMotor, int pinLed)
+void encenderMotorYLedSierra()
 {
-  digitalWrite(pinMotor, LOW);
-  digitalWrite(pinLed, LOW);
+  digitalWrite(RELE, HIGH);
+  digitalWrite(LED_SIERRA, HIGH);
 }
 
-long medirDistanciaUltrasonido(int triggerPin, int echoPin, bool estaEnMovimiento, long margenDeError, int pinPulsador)
+void detenerMotorYLedSierra()
+{
+  digitalWrite(RELE, LOW);
+  digitalWrite(LED_SIERRA, LOW);
+   Serial.println("Deteniendo el motor sierra");
+}
+
+//----------------------------------------------------
+// Otras Funcionalidades
+//----------------------------------------------------
+
+unsigned long medirDistanciaUltrasonido(unsigned int triggerPin, unsigned int echoPin, bool estaEnMovimiento, unsigned int pinPulsador)
 {
   digitalWrite(triggerPin, LOW);
   delayMicroseconds(2);
@@ -273,10 +437,15 @@ long medirDistanciaUltrasonido(int triggerPin, int echoPin, bool estaEnMovimient
   delayMicroseconds(10);
   digitalWrite(triggerPin, LOW);
   unsigned long duracion = pulseIn(echoPin, HIGH);
-  if(estaEnMovimiento)
+  if(estaEnMovimiento || pinPulsador != PULSADOR_SIERRA)
   {
-    duracion += pinPulsador == PULSADOR_IZQ ? margenDeError : -margenDeError;
+    duracion += pinPulsador == PULSADOR_IZQ ? MARGEN_DE_ERROR : -MARGEN_DE_ERROR;
   }
-  long distanciaCm = duracion / 57;
+  unsigned long distanciaCm = duracion / CONST_VELOCIDAD_SONIDO;
   return distanciaCm;
+}
+
+bool esPulsadorPresionado(unsigned int pinPulsador)
+{
+  return digitalRead(pinPulsador) == HIGH;
 }
